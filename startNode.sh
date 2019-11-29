de1bc7ec3b7a2767d868b91e7356281a086013af
Eric Badiere <ebadiere@gmail.com>
2019-11-28 21:36:56 -0700

    Initial commit

---
+++ startNode.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+docker start /chainlink-postgres
+cd ~/.chainlink-ropsten && docker run -p 6688:6688 -v ~/.chainlink-ropsten:/chainlink -it --env-file=.env smartcontract/chainlink local n
